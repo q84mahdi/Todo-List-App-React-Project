81c801d810bf13ebf34bffdff8cfba0caa8f133b
q84mahdi <ghasemi84mahdi@gmail.com>
2025-11-28 17:10:26 +0330

    Update main files to TS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { Provider } from "react-redux";
+import App from "./App";
 import store from "./Features/store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <App />
   </Provider>
